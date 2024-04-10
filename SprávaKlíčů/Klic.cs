490445e4038b75f1e2cc9bc38b51447728bff233
captangregor <vojtagregor@email.cz>
2024-04-10 19:16:20 +0200

    oop_hotovson

--- SprávaKlíčů/Klic.cs
+++ SprávaKlíčů/Klic.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 
 namespace SprávaKlíčů
 {
-    internal class Klic
+    public class Klic
     {
+        public int Cislo { get; set; }
+        public string NazevMistnosti { get; set; }
+        public string OznaceniDveri { get; set; }
+        public int PocetVyrobenychKusu { get; set; }
 
+        public int PocetVypujcenychKusu { get; set; }
+
+        public Klic(int cislo, string nazevMistnosti, string oznaceniDveri, int pocetVyrobenychKusu)
+        {
+            Cislo = cislo;
+            NazevMistnosti = nazevMistnosti;
+            OznaceniDveri = oznaceniDveri;
+            PocetVyrobenychKusu = pocetVyrobenychKusu;
+            PocetVypujcenychKusu = 0;
+        }
     }
 }
--- SprávaKlíčů/Zamestnanec.cs
+++ SprávaKlíčů/Zamestnanec.cs
@@ -6,8 +6,29 @@ using System.Threading.Tasks;
 
 namespace SprávaKlíčů
 {
-    internal class Zamestnanec
+    public class Zamestnanec
     {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public string Zkratka { get; set; }
 
+        public List<Klic> VypujceneKlice { get; set; } = new List<Klic>();
+
+        public Zamestnanec(string jmeno, string prijmeni, string zkratka)
+        {
+            Jmeno = jmeno;
+            Prijmeni = prijmeni;
+            Zkratka = zkratka.ToUpper(); 
+        }
+        public void PridatVypujcenyKlic(Klic klic)
+        {
+            VypujceneKlice.Add(klic);
+        }
+
+        public void OdebratVypujcenyKlic(Klic klic)
+        {
+            VypujceneKlice.Remove(klic);
+        }
     }
+
 }
